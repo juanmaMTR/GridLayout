30a77d464b2a5bf49cf9d798ce52101a1e7de04b
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2021-12-21 10:19:03 +0100

    Grid-Layout

---
+++ js/gridlayout.js
@@ -0,0 +1,7 @@
+/**
+ * @file gridlayout.js
+ * Uso del grid-layout
+ * @author Juan Manuel Toscano Reyes <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
+ * @license GPL V.3.0
+ */
+'use strict'
